2347c446e39b39c6a1d858d13c73e68a5bc72042
Seaton Ullberg <seatonullberg@gmail.com>
2020-07-19 16:47:42 -0400

    Split into lib and bin

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+pub mod energy;
+pub mod ensemble;
+pub mod force;
+pub mod potential;
+pub mod simulation;
+pub mod system;
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,3 @@
-mod energy;
-mod ensemble;
-mod force;
-mod potential;
-mod simulation;
-mod system;
-
 fn main() {
     println!("Hello, world!");
 }
