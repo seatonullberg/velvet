14473e3ed33b67becdccb26224843cca68b959c3
Seaton Ullberg <seatonullberg@gmail.com>
2020-07-21 00:47:23 -0400

    Add force implementation for each pair potential

--- src/potential/pair.rs
+++ src/potential/pair.rs
@@ -1,6 +1,8 @@
+use nalgebra::Vector3;
+
 pub trait PairPotential {
     fn energy(&self, r: f32) -> f32;
-    // fn force(&self, r: f32) -> f32;
+    fn force(&self, a: Vector3<f32>, b: Vector3<f32>) -> Vector3<f32>;
 }
 
 /// Lennard-Jones potential
@@ -20,6 +22,14 @@ impl PairPotential for LennardJones {
     fn energy(&self, r: f32) -> f32 {
         let term = (self.sigma / r).powi(6);
         4.0 * self.epsilon * (term * term - term)
+    }
+
+    fn force(&self, a: Vector3<f32>, b: Vector3<f32>) -> Vector3<f32> {
+        let r = (a - b).norm();
+        let term_a = (48.0 * self.sigma.powi(12) * self.epsilon) / r.powi(13);
+        let term_b = (24.0 * self.sigma.powi(6) * self.epsilon) / r.powi(7);
+        let force = term_a - term_b;
+        (force * (a - b)) / r
     }
 }
 
@@ -50,6 +60,16 @@ impl PairPotential for Mie {
             * (self.gamma_r / self.gamma_a).powf(self.gamma_a / (self.gamma_r - self.gamma_a));
         c * self.epsilon * (term_a - term_b)
     }
+
+    fn force(&self, a: Vector3<f32>, b: Vector3<f32>) -> Vector3<f32> {
+        let r = (a - b).norm();
+        let c = (self.gamma_r / (self.gamma_r - self.gamma_a))
+            * (self.gamma_r / self.gamma_a).powf(self.gamma_a / (self.gamma_r - self.gamma_a));
+        let term_a = (c * self.gamma_r * self.epsilon * (self.sigma / r).powf(self.gamma_r)) / r;
+        let term_b = (c * self.gamma_a * self.epsilon * (self.sigma / r).powf(self.gamma_a)) / r;
+        let force = term_a - term_b;
+        (force * (a - b)) / r
+    }
 }
 
 /// Morse potential
@@ -72,5 +92,13 @@ impl PairPotential for Morse {
         let term_a = f32::exp(-2.0 * self.a * (r - self.r_e));
         let term_b = 2.0 * f32::exp(-self.a * (r - self.r_e));
         self.d_e * (term_a - term_b)
+    }
+
+    fn force(&self, a: Vector3<f32>, b: Vector3<f32>) -> Vector3<f32> {
+        let r = (a - b).norm();
+        let term_a = f32::exp(-2.0 * self.a * (r - self.r_e));
+        let term_b = f32::exp(-self.a * (r - self.r_e));
+        let force = 2.0 * self.a * self.d_e * (term_a - term_b);
+        (force * (a - b)) / r
     }
 }
