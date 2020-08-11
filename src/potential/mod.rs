55be5af5e95ecbff926300c7136a740a2adadaeb
Seaton Ullberg <seatonullberg@gmail.com>
2020-08-10 20:50:01 -0400

    Add force evaluation for pair potentials

--- src/potential/mod.rs
+++ src/potential/mod.rs
@@ -1,3 +1,5 @@
+// TODO: break out the identical portions of `evaluate_energy` and `evaluate_force` into a separate function.
+
 pub mod pair;
 
 use crate::distance::distance;
@@ -49,6 +51,32 @@ impl<T: PairPotential> EnergyEvaluator for Potential<T> {
 
 impl<T: PairPotential> ForceEvaluator for Potential<T> {
     fn evaluate_force(&self, system: &System, index: usize) -> Vector3<f32> {
-        unimplemented!()
+        let atom = &system.atoms[index];
+        // NOTE: `self.symbols` is assumed to be sorted
+        let defined_symbols: Vec<&str> = self.symbols.iter().map(AsRef::as_ref).collect();
+        let mut force: Vector3<f32> = Vector3::zeros();
+        // iterate over all atoms in the system
+        for (i, atom_i) in system.atoms.iter().enumerate() {
+            // skip the atom of interest
+            if i == index {
+                continue;
+            }
+            // skip undefined symbol pairs
+            let mut current_symbols = vec![&atom.symbol, &atom_i.symbol];
+            current_symbols.sort();
+            if current_symbols != defined_symbols {
+                continue;
+            }
+            // calculate the distance
+            let dist = distance(system, &atom.position, &atom_i.position);
+            let mag = dist.norm();
+            // skip atoms beyond the cutoff radius
+            if mag > self.cutoff {
+                continue;
+            }
+            // add to the total force
+            force += (dist / mag) * self.evaluator.force(mag);
+        }
+        force
     }
 }
