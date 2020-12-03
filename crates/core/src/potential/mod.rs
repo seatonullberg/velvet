7629a1a2473daa6b3cb769fd2a61adf9e8b8fe79
Seaton Ullberg <seatonullberg@gmail.com>
2020-12-03 18:35:08 -0500

    Update potential module

--- crates/core/src/potential/mod.rs
+++ crates/core/src/potential/mod.rs
@@ -1,24 +1,10 @@
-pub mod angle;
-pub mod bond;
-pub mod coulomb;
-pub mod dihedral;
-pub mod improper;
 pub mod pair;
-
-use nalgebra::Vector3;
-
-use crate::system::System;
-
-/// Evaluates the forces acting on all atoms in the system.
-pub trait ForceEvaluator {
-    fn evaluate_forces(&self, system: &System) -> Vec<Vector3<f32>>;
-}
 
 /// Restrictions which can be applied to a potential.
 #[derive(Clone, Copy, Debug)]
-pub enum Scope {
-    /// Applies to all atoms in the system.
-    Global,
+pub enum Restriction {
+    /// No restrictions.
+    None,
     /// Applies only to atoms in separate molecules.
     Intermolecular,
     /// Applies only to atoms within the same molecule.
--- crates/core/src/potential/pair.rs
+++ crates/core/src/potential/pair.rs
@@ -1,7 +1,5 @@
-use nalgebra::Vector3;
-
-use crate::potential::{ForceEvaluator, Scope};
-use crate::system::System;
+use crate::potential::Restriction;
+use crate::system::element::Element;
 
 /// Required behaviors for a pairwise interatomic potential.
 pub trait PairPotential {
@@ -11,71 +9,13 @@ pub trait PairPotential {
     fn force(&self, r: f32) -> f32;
 }
 
-/// Pair potential with required metadata.
-pub struct PairPotentialData {
-    /// Heap allocated interatomic pair potential.
-    potential: Box<dyn PairPotential>,
-    /// Pair of species this potential applies to.
-    species: (usize, usize),
-    /// Applicability of the potential.
-    scope: Scope,
-    /// Cutoff radius within which the potential is applied.
+/// Pair potential meta data.
+#[derive(Copy, Clone, Debug)]
+pub struct PairMeta {
+    /// Applicable elements.
+    elements: (Element, Element),
+    /// Limitation to the applicability.
+    restriction: Restriction,
+    /// Cutoff radius.
     cutoff: f32,
-}
-
-impl ForceEvaluator for PairPotentialData {
-    fn evaluate_forces(&self, system: &System) -> Vec<Vector3<f32>> {
-        // start with all pairs that are in scope
-        let mut indices = system.pairs.get(&self.species).unwrap().clone();
-        indices.retain(|(i, j)| match self.scope {
-            Scope::Global => true,
-            Scope::Intermolecular => system.molecule_ids[*i] != system.molecule_ids[*j],
-            Scope::Intramolecular => system.molecule_ids[*i] == system.molecule_ids[*j],
-        });
-
-        // calculate distances between each pair
-        let distances: Vec<f32> = indices
-            .iter()
-            .map(|(i, j)| {
-                system
-                    .cell
-                    .distance(&system.positions[*i], &system.positions[*j])
-            })
-            .collect();
-
-        // calculate force magnitudes for each pair
-        let force_mags: Vec<f32> = distances
-            .iter()
-            .map(|r| {
-                if *r > self.cutoff {
-                    0 as f32
-                } else {
-                    self.potential.force(*r)
-                }
-            })
-            .collect();
-
-        // calculate unit vector directions between each pair
-        let directions: Vec<Vector3<f32>> = indices
-            .iter()
-            .map(|(i, j)| {
-                system
-                    .cell
-                    .direction(&system.positions[*i], &system.positions[*j])
-            })
-            .collect();
-
-        // calculate the force vector of each atom in the system
-        let mut forces: Vec<Vector3<f32>> = vec![Vector3::default(); system.size()];
-        indices
-            .iter()
-            .zip(force_mags.iter())
-            .zip(directions.iter())
-            .for_each(|(((i, j), &f), &dir)| {
-                forces[*i] += f * dir;
-                forces[*j] -= f * dir;
-            });
-
-        forces
-    }
 }
