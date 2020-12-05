d7544973fb12a834cd99019ef84bd8a3ea1b1a45
Seaton Ullberg <seatonullberg@gmail.com>
2020-12-05 15:07:04 -0500

    Split System and Potentials

--- crates/core/src/potential/mod.rs
+++ crates/core/src/potential/mod.rs
@@ -1,6 +1,22 @@
 pub mod pair;
 
+use crate::potential::pair::{PairPotentialEnum, PairPotentialMeta};
+
 pub trait Potential: Clone + Copy + Send + Sync {}
+
+pub struct Potentials {
+    pair: Vec<(PairPotentialEnum, PairPotentialMeta)>,
+    // --bond--
+    // --angle--
+    // --dihedral--
+    // --coulomb--
+}
+
+impl Potentials {
+    pub fn pairs(&self) -> impl Iterator<Item = &(PairPotentialEnum, PairPotentialMeta)> {
+        self.pair.iter()
+    }
+}
 
 /// Restrictions which can be applied to a potential.
 #[derive(Clone, Copy, Debug)]
--- crates/core/src/property.rs
+++ crates/core/src/property.rs
@@ -1,10 +1,11 @@
 use nalgebra::Vector3;
 
+use crate::potential::Potentials;
 use crate::system::System;
 
 /// Calculates a system-wide property.
 pub trait Property {
-    fn calculate(&self, system: &System) -> Output;
+    fn calculate(&self, system: &System, potentials: &Potentials) -> Output;
 }
 
 pub enum PropertyEnum {
@@ -13,10 +14,10 @@ pub enum PropertyEnum {
 }
 
 impl Property for PropertyEnum {
-    fn calculate(&self, system: &System) -> Output {
+    fn calculate(&self, system: &System, potentials: &Potentials) -> Output {
         match self {
-            PropertyEnum::Forces(f) => f.calculate(system),
-            PropertyEnum::External(e) => e.calculate(system),
+            PropertyEnum::Forces(f) => f.calculate(system, potentials),
+            PropertyEnum::External(e) => e.calculate(system, potentials),
         }
     }
 }
@@ -33,7 +34,7 @@ pub enum Output {
 pub struct Forces;
 
 impl Property for Forces {
-    fn calculate(&self, system: &System) -> Output {
+    fn calculate(&self, system: &System, potentials: &Potentials) -> Output {
         unimplemented!()
     }
 }
