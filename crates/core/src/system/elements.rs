afa91404cb24f16a38a8b5303f62ac407b8446b6
Seaton Ullberg <seatonullberg@gmail.com>
2020-12-14 00:26:35 -0500

    Add string parsing for Element

--- crates/core/src/system/elements.rs
+++ crates/core/src/system/elements.rs
@@ -1,9 +1,11 @@
 //! Elemental properties.
 
 use serde::{Deserialize, Serialize};
+use strum_macros::EnumString;
 
 /// Every element on the periodic table.
 #[derive(Clone, Copy, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(EnumString)]
 pub enum Element {
     /// Hydrogen
     H,
@@ -24,5 +26,17 @@ impl Element {
             Element::F => 18.998,
             Element::Ar => 39.948,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+    use super::Element;
+
+    #[test]
+    fn from_str() {
+        let hydrogen = Element::from_str("H").unwrap();
+        assert_eq!(Element::H, hydrogen)
     }
 }
