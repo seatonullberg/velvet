d88d4fec7be21c5c53e2f09f633f5218536d1028
Seaton Ullberg <seatonullberg@gmail.com>
2020-12-15 03:41:37 -0500

    Finalizing changes for velvet-core v0.2.0 release

--- crates/core/benches/thermostats.rs
+++ crates/core/benches/thermostats.rs
@@ -1,7 +1,7 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use velvet_convert::load_poscar;
 use velvet_core::integrators::{Integrator, VelocityVerlet};
-use velvet_core::thermostats::{Berendsen, Thermostat};
+use velvet_core::thermostats::{Berendsen, Thermostat, NoseHoover};
 use velvet_core::utils::{load_test_potentials, test_path};
 
 pub fn berendsen_benchmark(c: &mut Criterion) {
@@ -13,13 +13,36 @@ pub fn berendsen_benchmark(c: &mut Criterion) {
 
     let target = 1000 as f32;
     let mut berendsen = Berendsen::new(target, 2.0);
+    berendsen.setup(&sys);
 
     c.bench_function("berendsen argon", |b| {
         b.iter(|| {
+            berendsen.pre_integrate(&mut sys);
+            vv.integrate(&mut sys, &pots);
             berendsen.post_integrate(&mut sys);
         })
     });
 }
 
-criterion_group!(thermostats, berendsen_benchmark);
+pub fn nose_hoover_benchmark(c: &mut Criterion) {
+    let mut sys = load_poscar(test_path("argon.poscar"));
+    let pots = load_test_potentials("argon");
+
+    let mut vv = VelocityVerlet::new(1.0);
+    vv.setup(&sys, &pots);
+
+    let target = 1000 as f32;
+    let mut nose = NoseHoover::new(target, 1.01, 1.0);
+    nose.setup(&sys);
+
+    c.bench_function("nose hoover argon", |b| {
+        b.iter(|| {
+            nose.pre_integrate(&mut sys);
+            vv.integrate(&mut sys, &pots);
+            nose.post_integrate(&mut sys);
+        })
+    });
+}
+
+criterion_group!(thermostats, berendsen_benchmark, nose_hoover_benchmark);
 criterion_main!(thermostats);
