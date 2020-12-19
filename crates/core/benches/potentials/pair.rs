a1748234386691777d99bfb2713b3cb3e9067a90
Seaton Ullberg <seatonullberg@gmail.com>
2020-12-18 19:48:29 -0500

    Update benchmarks

--- crates/core/benches/potentials/pair.rs
+++ crates/core/benches/potentials/pair.rs
@@ -4,45 +4,43 @@ use velvet_core::potentials::pair::{Harmonic, LennardJones, Mie, Morse, PairPote
 
 static DISTANCE: f32 = 1.0;
 
-pub fn pair_potentials_energy_benchmark(c: &mut Criterion) {
-    let mut group = c.benchmark_group("pair_potentials_energy");
-
+pub fn lennard_jones_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("lennard_jones");
     let lj = LennardJones::new(0.8, 2.0);
-    group.bench_function("lennard_jones", |b| b.iter(|| lj.energy(DISTANCE)));
-
-    let harmonic = Harmonic::new(50.0, 2.0);
-    group.bench_function("harmonic", |b| b.iter(|| harmonic.energy(DISTANCE)));
-
-    let mie = Mie::new(0.8, 2.0, 6.0, 12.0);
-    group.bench_function("mie", |b| b.iter(|| mie.energy(DISTANCE)));
-
-    let morse = Morse::new(1.3, 4.0, 2.0);
-    group.bench_function("morse", |b| b.iter(|| morse.energy(DISTANCE)));
-
+    group.bench_function("energy", |b| b.iter(|| lj.energy(DISTANCE)));
+    group.bench_function("force", |b| b.iter(|| lj.force(DISTANCE)));
     group.finish();
 }
 
-pub fn pair_potentials_force_benchmark(c: &mut Criterion) {
-    let mut group = c.benchmark_group("pair_potentials_force");
-
-    let lj = LennardJones::new(0.8, 2.0);
-    group.bench_function("lennard_jones", |b| b.iter(|| lj.force(DISTANCE)));
-
+pub fn harmonic_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("harmonic");
     let harmonic = Harmonic::new(50.0, 2.0);
-    group.bench_function("harmonic", |b| b.iter(|| harmonic.force(DISTANCE)));
+    group.bench_function("energy", |b| b.iter(|| harmonic.energy(DISTANCE)));
+    group.bench_function("force", |b| b.iter(|| harmonic.force(DISTANCE)));
+    group.finish();
+}
 
+pub fn mie_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("mie");
     let mie = Mie::new(0.8, 2.0, 6.0, 12.0);
-    group.bench_function("mie", |b| b.iter(|| mie.force(DISTANCE)));
+    group.bench_function("energy", |b| b.iter(|| mie.energy(DISTANCE)));
+    group.bench_function("force", |b| b.iter(|| mie.force(DISTANCE)));
+    group.finish();
+}
 
+pub fn morse_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("morse");
     let morse = Morse::new(1.3, 4.0, 2.0);
-    group.bench_function("morse", |b| b.iter(|| morse.force(DISTANCE)));
-
+    group.bench_function("energy", |b| b.iter(|| morse.energy(DISTANCE)));
+    group.bench_function("force", |b| b.iter(|| morse.force(DISTANCE)));
     group.finish();
 }
 
 criterion_group!(
     pair_potentials,
-    pair_potentials_energy_benchmark,
-    pair_potentials_force_benchmark
+    lennard_jones_benchmark,
+    harmonic_benchmark,
+    mie_benchmark,
+    morse_benchmark,
 );
 criterion_main!(pair_potentials);
