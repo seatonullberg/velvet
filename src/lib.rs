ca116264a834f6d425ff442d7d87176f69e7310f
Seaton Ullberg <seatonullberg@gmail.com>
2023-05-01 18:47:04 -0400

    Initial commit of v0.5 rewrite.

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,3 @@
-//! Velvet is a classical atomistic simulation engine with a focus on user-friendliness and extensibility.
-//!
-//! This project is largely a learning exercise, but as development continues I hope to accomplish the following goals:
-//!
-//! * Extensibility via user-defined plugin modules
-//! * Optimized single CPU performace with multithreading and SIMD support
-//! * Implement a wide variety of interatomic potentials
-//! * Molecular dynamics, Monte Carlo, and energy minimization routines
-//! * Visualization tools to analyze simulation results
-//! * Support importing and exporting data in popular external formats
-
-/// `use velvet::prelude::*;` to import common components.
-pub mod prelude {
-    pub use velvet_core::prelude::*;
-    pub use velvet_external_data::prelude::*;
-}
-
-pub mod core {
-    //! Core feature library for the Velvet simulation engine.
-    pub use velvet_core::*;
-}
-
-pub mod external_data {
-    //! Utilities to import and export data in external formats.
-    pub use velvet_external_data::*;
+fn main() {
+    println!("Hello, world!");
 }
